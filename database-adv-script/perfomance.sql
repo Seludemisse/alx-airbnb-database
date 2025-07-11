25d29ca9faa9f1d463def6ce443807d9182480b2
Seludemisse <seludemisse12@gmail.com>
2025-07-11 01:48:45 -0700

    Fix Task 4: Add WHERE and AND clause to filtered query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,4 @@
+
 EXPLAIN ANALYZE
 SELECT
     bookings.id AS booking_id,
@@ -12,6 +13,7 @@ JOIN users ON bookings.user_id = users.id
 JOIN properties ON bookings.property_id = properties.id
 JOIN payments ON bookings.id = payments.booking_id;
 
+
 EXPLAIN ANALYZE
 SELECT
     b.id AS booking_id,
@@ -25,3 +27,19 @@ FROM
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.id = pay.booking_id;
+
+EXPLAIN ANALYZE
+SELECT
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount
+FROM
+    bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.id = pay.booking_id
+WHERE b.start_date >= '2024-01-01'
+  AND pay.amount > 100;
